33a7e5c7a5c5fc43cf44f992b6a8a1b4e5c9363f
Rifat <rifatcse09@gmail.com>
2025-08-14 15:13:44 +0600

    :up: Update List button

--- src/views/Organizer/List.vue
+++ src/views/Organizer/List.vue
@@ -54,13 +54,13 @@
                 <td class="px-6 py-4 whitespace-nowrap flex space-x-2">
                   <router-link
                     :to="`/organizer-management/edit/${organizer.id}`"
-                    class="text-blue-600 hover:text-blue-900"
+                    class="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1"
                   >
                     Edit
                   </router-link>
                   <button
-                    class="text-red-600 hover:text-red-900"
                     @click="confirmDelete(organizer.id)"
+                    class="px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-1"
                   >
                     Delete
                   </button>
